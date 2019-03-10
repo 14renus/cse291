6656be710f07aa73fa2a3e12787f3f94bc4906c7
res001 <res001@node.res001-qv48106.orion-pg0.wisc.cloudlab.us>
2019-03-10 15:48:11 -0500

    set up script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+sudo apt-get update
+sudo apt-get install curl
+
+sudo apt-get install python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+
+
+
+pip install numpy pandas torch 
+
+
+
